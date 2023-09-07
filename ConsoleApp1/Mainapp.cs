b886bba53f09015eb343897745caa1369e96c69a
codefug <86544979+codefug@users.noreply.github.com>
2023-09-07 10:45:11 +0900

    .

---
+++ ConsoleApp1/Mainapp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class MainApp {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args) {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법: Hello.exe 이름");
+                return;
+            }
+            Console.WriteLine("Hello, {0}!", args[0]); // Hello, World World를 프롬프트에 출력
+        } 
+    } 
+}
