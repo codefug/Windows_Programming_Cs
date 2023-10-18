12edff825877df41ad2e48bb1aa2d31b38efacf4
codefug <robot9917@naver.com>
2023-10-18 22:43:17 +0900

    add Practice note

---
+++ Practice Note for Mid Term/Practice Note for Mid Term/Practice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice // Note: actual namespace depends on the project name.
+{
+    internal class Practice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
