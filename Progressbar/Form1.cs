3d71314f13e793af5c8a5b8b0a44afcb089c2aec
codefug <86544979+codefug@users.noreply.github.com>
2023-11-20 23:16:59 +0900

    add week 12

---
+++ Progressbar/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progressbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateEventLabels(string msg, int x, int y, MouseEventArgs e)
+        {
+            string message = string.Format("{0} X:{1}, Y:{2}", msg, x, y);
+            string eventMsg = DateTime.Now.ToShortTimeString();
+            eventMsg += " " + message;
+            listBox1.Items.Insert(0, eventMsg);
+            listBox1.TopIndex = 0;
+            string mouseInfo;
+            if (e != null)
+            {
+                mouseInfo = string.Format("Clicks: {0}, Delta: {1}, " + "Buttons: {2}", e.Clicks, e.Delta, e.Button.ToString());
+            }
+            else
+            {
+                mouseInfo = string.Format("Clicks: {0}", msg);
+            }
+            label1.Text = mouseInfo;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(Form)MouseClick", e.X, e.Y, null);
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(Form)MouseDoubleClick", e.X, e.Y, null);
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            Point mousePoint = PointToClient(MousePosition);
+            UpdateEventLabels("(Form)MouseEnter", mousePoint.X, mousePoint.Y, null);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(Form)MouseDown", e.X, e.Y, e);
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            Point mousePoint = PointToClient(MousePosition);
+            UpdateEventLabels("(Form)MouseHover", mousePoint.X, mousePoint.Y, null);
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(Form)MouseHover",e.X,e.Y, e);
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        { 
+            Point mousePoint = PointToClient(MousePosition);
+            UpdateEventLabels("(Form)MouseLeave", mousePoint.X, mousePoint.Y, null);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(Form)MouseMove", e.X, e.Y, e);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point mousePoint = PointToClient(MousePosition);
+            UpdateEventLabels("(Form)MouseUp", e.X, e.Y, e);
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            UpdateEventLabels("(ListBox)MouseDown", e.X, e.Y, e);
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Point mousePoint = PointToClient(MousePosition);
+            UpdateEventLabels("(ListBox)DoubleClick", mousePoint.X, mousePoint.Y, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
---
+++ upDownControl/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace upDownControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Log 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0*0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "√0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 46);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(439, 25);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 25);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 194);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 256);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(330, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(535, 46);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 276);
+            this.trackBar1.TabIndex = 11;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(597, 59);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(334, 56);
+            this.trackBar2.TabIndex = 12;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(597, 121);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 334);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NumericUpDownApp";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
