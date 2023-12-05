0d071ebdbeacffffc084e35549fd08802bb76b46
codefug <robot9917@naver.com>
2023-12-06 00:24:52 +0900

    .

---
+++ finalExamProject/form/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form
+{
+    public partial class Form1 : Form
+    {
+        DateTime startDateTime;
+        public Form1()
+        {
+            startDateTime = DateTime.Now;
+            InitializeComponent();
+        }
+        public DateTime GetStartDateTime()
+        {
+            return startDateTime;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetStartDateTime().ToString());
+        }
+
+    }
+}
